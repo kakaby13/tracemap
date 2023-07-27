f425b9d7e14ed4fe93959029138893061fa50b7f
Vlad Gavrilchik <kakaby13@gmail.com>
2023-07-27 18:29:14 +0300

    Scanner core added, bugfix and refactoring

---
+++ TraceMap/TraceMap/Scaner/GraphHelper.cs
@@ -0,0 +1,96 @@
+﻿using TraceMap.Models;
+using TraceMap.Utils;
+
+namespace TraceMap.Scaner;
+
+public static class GraphHelper
+{
+    public static Node MapToGraph(this List<string> rawNodes)
+    {
+        rawNodes.Reverse();
+        var currentNode = new Node();
+
+        foreach (var rawNode in rawNodes)
+        {
+            currentNode.Value = rawNode;
+            var newNode = new Node
+            {
+                ChildrenNode = new List<Node> { currentNode }
+            };
+            currentNode = newNode;
+        }
+
+        return currentNode;
+    }
+
+    public static Node MergeRoutes(this List<Node> nodes)
+    {
+        return nodes
+            .MergeRoot()
+            .JoinChildrenNodes();
+    }
+
+    private static Node JoinChildrenNodes(this Node node)
+    {
+        if (node.ChildrenNode.IsNullOrEmpty())
+        {
+            return node;
+        }
+        var result = new Node
+        {
+            Value = node.Value,
+            ChildrenNode = new List<Node>()
+        };
+
+        var gropedNodes = node.ChildrenNode!
+            .GroupBy(c => c.Value)
+            .ToList();
+        
+        foreach (var sameValue in gropedNodes.Where(c => c.Count() > 1))
+        {
+            var subResult = new Node
+            {
+                Value = sameValue.Key,
+                ChildrenNode = new List<Node>()
+            };
+
+            subResult.ChildrenNode = sameValue
+                .SelectMany(c => c.ChildrenNode!)
+                .ToList();
+
+            result.ChildrenNode.Add(subResult);
+        }
+
+        var uniqueNodes = gropedNodes
+            .Where(c => c.Count() == 1)
+            .SelectMany(c => c.ToList());
+        
+        result.ChildrenNode.AddRange(uniqueNodes);
+        
+        var joinedChildren = result
+            .ChildrenNode.Select(JoinChildrenNodes)
+            .ToList();
+
+        result.ChildrenNode = joinedChildren;
+
+        return result;
+    }
+
+    private static Node MergeRoot(this IEnumerable<Node> nodes)
+    {
+        return new Node
+        {
+            Value = "root",
+            ChildrenNode = nodes.SelectMany(c => c.ChildrenNode!).ToList()
+        };
+    }
+
+    public static int CalculateDepth(this Node node, int currentDepth = 1)
+    {
+        return node.ChildrenNode.IsNullOrEmpty()
+            ? currentDepth
+            : node.ChildrenNode!
+                .Select(c => CalculateDepth(c, currentDepth + 1))
+                .Max();
+    }
+}
---
+++ TraceMap/TraceMap/Scaner/ScanerCore.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace TraceMap.Scaner;
+
+public class ScanerCore
+{
+    public List<string> Scan(string hostname)
+    {
+        var result = new List<IPAddress>();
+        const int timeout = 10000;
+        const int maxHopCount = 255;
+        
+        using var pinger = new Ping();
+        for (var timeToLive = 1; timeToLive <= maxHopCount; timeToLive++)
+        {
+            var options = new PingOptions(timeToLive, true);
+            var reply = pinger.Send(hostname, timeout, GetRandomContent(), options);
+
+            if (reply.Status is IPStatus.Success or IPStatus.TtlExpired)
+            {
+                result.Add(reply.Address);
+            }
+
+            if (reply.Status != IPStatus.TtlExpired && reply.Status != IPStatus.TimedOut)
+            {
+                break;
+            }
+        }
+
+        return result
+            .Select(c => c.ToString())
+            .ToList();
+    }
+
+    private static byte[] GetRandomContent()
+    {
+        const int bufferSize = 255;
+        var buffer = new byte[bufferSize];
+        new Random().NextBytes(buffer);
+
+        return buffer;
+    }
+}
